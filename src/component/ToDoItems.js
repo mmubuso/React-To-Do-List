4e84b04800f14397816a754ea49f79eb76d8cbf0
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-17 10:02:16 -0400

    updated inline styles

--- src/component/ToDoItems.js
+++ src/component/ToDoItems.js
@@ -4,9 +4,25 @@ export default class ToDoItems extends Component {
 
     
     render() {
+
+        //CSS Styles
+        //Style list items
+        const listItemStyle = {
+            borderRadius: '0.4em',
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            fontSize: '2em',
+            padding: '0.4em',
+            margin: '0.5em',
+            marginBottom: '0.2em',
+            color: 'white',
+            boxShadow: "0 0 11px rgba(33,33,33,.2)"
+        }
         return (
             <div>
-                <li>{this.props.item}</li>
+                <li 
+                style={listItemStyle}
+                onClick={()=> this.props.deleteItem(this.props.index)}>
+                {this.props.item}</li>
             </div>
         )
     }
